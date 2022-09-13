86ca480099e882bc6e76552abb781ae98522f7a0
Glenn Halls <gjhalls@gmail.com>
2022-09-13 18:03:00 +1000

    3-6: Use for loops

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,42 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if (true || false) {
-            System.out.println("only one condition needs to be true");
+        for (int i = 0; i < 25; i++) {
+            System.out.println("the square of " + i + " is " + Math.pow(i,2));
         }
 
-        if (true && false) {
-            System.out.println("this can never be true");
-        }
+        /*
+        1  2  3  4  5  6  7  8  9  10  11  12
+        2  4  6  8  10 12 14 16 18 20  22  24
+         */
 
-        int myMaxLimit = 20;
-        int myMinLimit = 5;
-
-        int myValue = 19;
-
-        if (myValue < myMaxLimit && myValue > myMinLimit) {
-            System.out.println("the value " + myValue + " is between " + myMinLimit + " and " + myMaxLimit);
-        } else {
-            System.out.println("the value " + myValue + " is not between " + myMinLimit + " and " + myMaxLimit);
-        }
-
-        int myMaxXLimit = 5;
-        int myMinXLimit = 1;
-        int myMaxYLimit = 3;
-        int myMinYLimit = 1;
-
-        int myXValue = 4;
-        int myYValue = 2;
-
-        if ((myXValue < myMaxXLimit && myXValue > myMinXLimit) && (myYValue < myMaxYLimit && myYValue > myMinYLimit)) {
-            System.out.println("hooray, x and y are within range!!");
-        } else {
-            System.out.println("boo, one or both of the x and y values are outside the range");
-        }
-
-        boolean myBoolean = false;
-        if (!myBoolean) {
-            System.out.println("NOT false is true");
+        for (int r = 1; r <= 12; r++) {
+            String tableRow = "";
+            for (int c = 1; c <= 12; c++) {
+                int product = r*c;
+                String productString = Integer.toString(product) + "\t";
+                tableRow += productString;
+            }
+            System.out.println(tableRow);
         }
     }
 }
