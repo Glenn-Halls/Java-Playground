a835a0216fc3eb2890f57a8674200e39a2ef866f
Glenn Halls <gjhalls@gmail.com>
2022-09-13 19:27:13 +1000

    3-8: Use loops with arrays

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,14 @@ public class Main {
     public static void main(String[] args) {
 
         String[] myArray = new String[] {"Doug", "Mike", "Janet", "Matt"};
-        System.out.println(myArray[2]);
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println("Hello, " + myArray[i]);
+        }
+
+        for (String element : myArray) {
+            System.out.println("Goodbye, " + element);
+        }
 
     }
 }
