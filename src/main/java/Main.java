556c6753045f13eb6e50a2981c10f9fadda6454d
Glenn Halls <gjhalls@gmail.com>
2022-09-14 10:36:36 +1000

    4-1: Why everything is an object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,10 @@
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
+        // int myValue is short for...
+        int myValue = 5;
+        int anotherValue = new Integer(5);
+        Integer myOtherValue = new Integer(5);
 
-        String[] myArray = new String[] {"Doug", "Mike", "Janet", "Matt"};
-        for (String element : myArray) System.out.println(element);
-
-        myArray[0] = "Tim";
-        for (String element : myArray) System.out.println(element);
-
-        ArrayList<String> myList = new ArrayList<>();
-        myList.add("Rocket");
-        myList.add("Scout");
-        System.out.println(myList);
-
-        for (String element : myList) System.out.println(element);
-        System.out.println(myList.size());
-
-        myList.add(1, "Hoover");
-        System.out.println(myList);
-
-        myList.remove("Scout");
-        System.out.println(myList);
-
-        myList.remove(1);
-        System.out.println(myList);
-
-        int al = myArray.length;
-        System.out.println(al);
     }
 }
