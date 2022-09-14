c8dfa83f1dfc2eddd1f407965afd028e3cbaaddd
Glenn Halls <gjhalls@gmail.com>
2022-09-14 10:40:10 +1000

    4-2: Why everything is a class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        // int myValue is short for...
-        int myValue = 5;
-        int anotherValue = new Integer(5);
-        Integer myOtherValue = new Integer(5);
+
+        String myString = new String("Doug");
+        int stringLength = myString.length();
+
 
     }
 }
