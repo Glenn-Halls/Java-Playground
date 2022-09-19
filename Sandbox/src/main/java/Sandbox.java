32b2da28edcd4179ae42a3823028cb32abd954ce
Glenn Halls <gjhalls@gmail.com>
2022-09-19 11:26:31 +1000

    Week 6: for loop

--- Sandbox/src/main/java/Sandbox.java
+++ Sandbox/src/main/java/Sandbox.java
@@ -5,23 +5,20 @@ public class Sandbox {
     public static void main(String[] args) {
 
         int number;
-        int counter = 0;
         int total = 0;
-
+        double average;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         number = input.nextInt();
 
-        do {
+        for (int counter = 1; counter <= number; counter++) {
             total += counter;
-            System.out.print(counter + ", ");
-            counter++;
-        } while (counter <= number);
+            System.out.print(counter);
+        }
 
-        System.out.println("\nTotal = " + total);
+        average = total / (double)number;
 
-
+        System.out.println("The total of the numbers from 1 to " + number +  " is " + total + " and the average is " + average);
     }
-
 }
