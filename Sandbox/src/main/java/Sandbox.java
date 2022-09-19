1ae929e14c8f702780f5e91b091eaef5251cbabe
Glenn Halls <gjhalls@gmail.com>
2022-09-19 14:08:30 +1000

    Week 6: break statements

--- Sandbox/src/main/java/Sandbox.java
+++ Sandbox/src/main/java/Sandbox.java
@@ -4,23 +4,29 @@ public class Sandbox {
 
     public static void main(String[] args) {
 
-        int levels;
+        int age;
+        String text;
         Scanner input = new Scanner(System.in);
 
-        System.out.print("How many levels should the tree have?: ");
-        levels = input.nextInt();
-
-        System.out.println("\n\n\n");
-
-        for (int level = 0; level < levels; level++) {
-            String branch = "*";
-            String space = "";
-            for (int stars = 0; stars < level; stars++) branch += "**";
-            for (int spaces = level + 1; spaces < levels; spaces++) space += " ";
-            branch = space + branch;
-
-            System.out.println(branch);
+        for (;;) {
+            System.out.print("\n\nEnter your age: ");
+            age = input.nextInt();
+            if (age > 70) {
+                text = "You belong to the Greatest Generation";
+            } else if (age > 41) {
+                text = "You belong to the Baby Boomers";
+            } else if (age > 38) {
+                text = "You belong to generation X";
+            } else if (age > 21) {
+                text = "You belong to generation Y";
+            } else if (age > 6) {
+                text = "You belong to generation Z";
+            } else if (age >= 0) {
+                text = "You belong to generation Alpha";
+            } else if (age != -1) {
+                text = "That is not a valid answer";
+            } else break;
+            System.out.print(text);
         }
-
     }
 }
