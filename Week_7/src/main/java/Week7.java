c74fa8809e0549a4e8c19fb3c259f5b78d614b51
Glenn Halls <gjhalls@gmail.com>
2022-09-26 10:59:46 +1000

    Week 7: Math.random()

--- Week_7/src/main/java/Week7.java
+++ Week_7/src/main/java/Week7.java
@@ -1,10 +1,12 @@
 import example.ExamScores;
+import example.Random;
 
 public class Week7 {
 
     public static void main(String[] args) {
 
-        ExamScores.run();
+//        ExamScores.run();
+        Random.run();
 
     }
 }
